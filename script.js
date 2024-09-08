2f501cb42ff932b270a250b1ac7299b0d39a0196
theaviralsharma <beingaviralsharma@gmail.com>
2024-09-09 02:25:31 +0530

    Welcome Page Added

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+var tl = gsap.timeline({scrollTrigger:{
+  trigger: "#main",
+  start: "50% 50%",
+  end: "100% 50%",
+  scrub:2,
+  pin:true
+}})
+
+tl
+.to("#top",{
+  top: "-50%"
+},'a')
+.to("#bottom",{
+  bottom: "-50%"
+},'a')
+.to("#top-h",{
+  top: "80%"
+},'a')
+.to("#bottom-h",{
+  bottom: "-40%"
+},'a')
